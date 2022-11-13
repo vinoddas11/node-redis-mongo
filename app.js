b16d2d02cadfa10602f4dfa613bde146c03baa27
ahildarshan <103110975+ahildarshan@users.noreply.github.com>
2022-11-13 15:41:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 // MONGODB SETUP
 
-mongoose.connect('mongodb://3.110.32.172:27017/redisdemo',{
+mongoose.connect('mongodb://13.126.245.133:27017/redisdemo',{
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
